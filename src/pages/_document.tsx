759a8a9b0be5eb1a6356449476d6d511ac75d812
HenryMoore-25 <toyinazeez2021@gmail.com>
2026-04-30 09:51:50 +0100

    preview shared social added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="dark">
-      <Head />
+      <Head>
+        <title>eventramarket</title>
+        <meta name="description" content="" />
+        <meta property="og:url" content="https://eventramarket.xyz/markets" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="eventramarket" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="/eventra.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="eventramarket.xyz" />
+        <meta property="twitter:url" content="https://eventramarket.xyz/markets" />
+        <meta name="twitter:title" content="eventramarket" />
+        <meta name="twitter:description" content="" />
+        <meta name="twitter:image" content="/eventra.jpg" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
