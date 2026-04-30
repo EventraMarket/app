f7064d30a10357383ee719b1067e640909fef14e
HenryMoore-25 <toyinazeez2021@gmail.com>
2026-04-30 10:54:26 +0100

    preview shared social added and fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,25 @@ export default function Document() {
     <Html lang="en" className="dark">
       <Head>
         <title>eventramarket</title>
-        <meta name="description" content="" />
-        <meta property="og:url" content="https://eventramarket.xyz/markets" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="eventramarket.xyz"/>
+         <meta property="twitter:url" content="https://eventramarket.xyz/" />
+         <meta name="twitter:title" content="eventramarket" />
+         <meta name="twitter:description"
+    content="" />
+       <meta name="twitter:image" content="https://eventramarket.xyz/eventra.jpg" />
+        {/* <meta name="description" content="" />
+        <meta property="og:url" content="https://eventramarket.xyz/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="eventramarket" />
         <meta property="og:description" content="" />
         <meta property="og:image" content="https://eventramarket.xyz/eventra.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="eventramarket.xyz" />
-        <meta property="twitter:url" content="https://eventramarket.xyz/markets" />
+        <meta property="twitter:url" content="https://eventramarket.xyz/" />
         <meta name="twitter:title" content="eventramarket" />
         <meta name="twitter:description" content="" />
-        <meta name="twitter:image" content="https://eventramarket.xyz/eventra.jpg" />
+        <meta name="twitter:image" content="https://eventramarket.xyz/eventra.jpg" /> */}
       </Head>
       <body className="antialiased">
         <Main />
